7318f33af22207521c9d04b4e99a8eccaaafe4bf
Fill <tima.filicheff1@yandex.ru>
2023-07-14 14:28:38 +0300

    -

---
+++ handlers/admin/read_file_with_command.py
@@ -0,0 +1,12 @@
+from aiogram import types, Dispatcher
+from create_bot import bot
+
+
+async def all_command(message: types.Message):
+    with open('all_bot_command', encoding='utf-8') as file:
+        content = file.read()
+    await message.reply(content)
+
+
+def register_handlers_command(disp: Dispatcher):
+    disp.register_message_handler(all_command, commands='help')
