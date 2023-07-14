de94848ead1e18122d8c65b88009025da7d905b8
Fill <tima.filicheff1@yandex.ru>
2023-07-14 14:31:42 +0300

    Изменил название команд

--- handlers/client/repeat_text.py
+++ handlers/client/repeat_text.py
@@ -1,17 +1,50 @@
+from aiogram.dispatcher import FSMContext
+from aiogram.dispatcher.filters import Text
+from aiogram.dispatcher.filters.state import State, StatesGroup
 from aiogram import types, Dispatcher
-from create_bot import bot
 
 
-# @dp.message_handler(commands=['start', 'help'])
-async def command_start(message: types.Message):
-    await bot.send_message(message.from_user.id, 'Здарова бандиты')
+class FSMAdmin(StatesGroup):
+    """ Создаем состояния """
+    text = State()  # Состояние текста
+    number = State()  # Состояние числового значения
 
 
-# @dp.message_handler(commands=['Напомни'])
-async def remind_text(message: types.Message):
-    await message.answer('Введите текст, который нужно напомнить')
+async def repeat_text_start(message: types.Message):
+    await FSMAdmin.text.set()  # Ожидаем состояние 'text'
+    await message.reply('Введите текст')
 
 
-def register_handlers_client(disp: Dispatcher):
-    disp.register_message_handler(command_start, commands=['start', 'help'])
-    disp.register_message_handler(remind_text, commands=['Напомни'])
+async def load_text(message: types.Message, state: FSMContext):
+    async with state.proxy() as data:
+        data['text'] = message.text  # Заполняем состояние 'text' введенным текстом
+    await FSMAdmin.next()  # Ожидаем следующее состояние 'number'
+    await message.reply('Введите число повторов')
+
+
+async def load_number(message: types.Message, state: FSMContext):
+    async with state.proxy() as data:
+        try:
+            data['number'] = int(message.text)  # Проверяем, чтобы сообщение было числом
+            await message.reply((data['text'] + ' ') * data['number'])  # Выводим несколько раз
+            await state.finish()
+        except:
+            await message.reply('Введите число')
+
+
+async def cancel_handler(message: types.Message, state: FSMContext):
+    """ Выполняем выход с помощью команды/слова 'отмена' """
+    current_state = await state.get_state()
+    if current_state is None:
+        return
+    await state.finish()
+    await message.reply('Хорошо')
+
+
+def register_handlers_repeat_text(disp: Dispatcher):
+    """ Регистрируем handlers """
+    disp.register_message_handler(repeat_text_start, commands='repeat')
+    disp.register_message_handler(cancel_handler, state='*', commands='cancel')
+    # disp.register_message_handler(cancel_handler, Text(equals='отмена', ignore_case=True), state='*')
+    disp.register_message_handler(load_text, content_types=['text'], state=FSMAdmin.text)
+    disp.register_message_handler(load_number, state=FSMAdmin.number)
