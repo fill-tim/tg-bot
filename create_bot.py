c43f853b17d65a6d3e9394cb543d02ffd1b6e328
Fill <tima.filicheff1@yandex.ru>
2023-07-17 10:22:30 +0300

    добавил 'MemoryStorage'

--- create_bot.py
+++ create_bot.py
@@ -1,9 +1,12 @@
 from aiogram import Bot
 from aiogram.dispatcher import Dispatcher
 import os
+from aiogram.contrib.fsm_storage.memory import MemoryStorage
 
 # PROXY_URL = 'http://proxy.server:3128'
 # bot = Bot(token=TOKEN, proxy=PROXY_URL)
 
+storage = MemoryStorage()
+
 bot = Bot(token=os.getenv('TOKEN'))
-dp = Dispatcher(bot)
+dp = Dispatcher(bot, storage=storage)
