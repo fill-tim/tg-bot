18404c42395d743574263b67aecbe766c7e69653
Fill <tima.filicheff1@yandex.ru>
2023-07-17 10:21:40 +0300

    добавил метод 'on_startup' в котором запускается создание бд

--- bot_telegram.py
+++ bot_telegram.py
@@ -1,10 +1,20 @@
 from aiogram.utils import executor
 from create_bot import dp
+from data_base import sqlite_db
 
-from handlers import client, admin, other
 
-client.register_handlers_client(dp)
-# client.register_handlers_admin(dp)
+async def on_startup(_):
+    print('Работает')
+    sqlite_db.start_db()
+
+
+from handlers.client import repeat_text, send_voice_message
+from handlers.admin import read_file_with_command
+from handlers import other
+
+repeat_text.register_handlers_repeat_text(dp)
+send_voice_message.register_handlers_voice_message(dp)
+read_file_with_command.register_handlers_command(dp)
 other.register_handlers_other(dp)
 
-executor.start_polling(dp, skip_updates=True)
+executor.start_polling(dp, skip_updates=True, on_startup=on_startup)
