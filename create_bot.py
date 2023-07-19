cbbd8161537305fe3b7d5ad10b22800b4829d7c3
Fill <tima.filicheff1@yandex.ru>
2023-07-19 11:53:28 +0300

    создание объекта Client

--- create_bot.py
+++ create_bot.py
@@ -2,6 +2,7 @@ from aiogram import Bot
 from aiogram.dispatcher import Dispatcher
 import os
 from aiogram.contrib.fsm_storage.memory import MemoryStorage
+from pyrogram import Client
 
 # PROXY_URL = 'http://proxy.server:3128'
 # bot = Bot(token=TOKEN, proxy=PROXY_URL)
@@ -10,3 +11,7 @@ storage = MemoryStorage()
 
 bot = Bot(token=os.getenv('TOKEN'))
 dp = Dispatcher(bot, storage=storage)
+api_id = 24360033
+api_hash = 'ae42a455f97b7c0221d4f91fa11fd8a2'
+bot_token = os.getenv('TOKEN')
+app = Client(name='GariginBot', api_id=api_id, api_hash=api_hash, bot_token=bot_token)
