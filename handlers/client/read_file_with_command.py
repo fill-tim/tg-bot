607e1b88a7e3fff5abb908067a683f4fbe714c9e
Fill <tima.filicheff1@yandex.ru>
2023-07-19 11:56:10 +0300

    перенесли файл

--- handlers/client/read_file_with_command.py
+++ handlers/client/read_file_with_command.py
@@ -1,11 +1,15 @@
 from aiogram import types, Dispatcher
 
+from create_bot import bot
+from key_boards import kb_client
 
-async def all_command(message: types.Message):
+
+async def output_all_command(message: types.Message):
     with open('all_bot_command', encoding='utf-8') as file:
         content = file.read()
     await message.reply(content)
+    # await bot.send_message(chat_id=message.chat.id, text=content, reply_markup=kb_client)
 
 
 def register_handlers_command(disp: Dispatcher):
-    disp.register_message_handler(all_command, commands='help')
+    disp.register_message_handler(output_all_command, commands='help')
