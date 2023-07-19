5783cfd0930232e83a21e498f31621d7dd9a4db2
Fill <tima.filicheff1@yandex.ru>
2023-07-19 11:52:42 +0300

    зарегистрировали новые handlers

--- bot_telegram.py
+++ bot_telegram.py
@@ -1,18 +1,17 @@
 from aiogram.utils import executor
-from create_bot import dp
+from create_bot import dp, app
 from data_base import sqlite_db
 
 
 async def on_startup(_):
-    print('Работает')
-    sqlite_db.start_db()
+    sqlite_db.create_db()
+    await app.start()
 
-
-from handlers.client import repeat_text, send_voice_message
-from handlers.admin import read_file_with_command
+from handlers.client import repeat_text, send_voice_message, find_bot, read_file_with_command
 from handlers import other
 
 repeat_text.register_handlers_repeat_text(dp)
+find_bot.register_handlers_find_bot(dp)
 send_voice_message.register_handlers_voice_message(dp)
 read_file_with_command.register_handlers_command(dp)
 other.register_handlers_other(dp)
