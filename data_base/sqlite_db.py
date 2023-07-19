3f3bf7ec57e5bb65296a495374dc1dda31052857
Fill <tima.filicheff1@yandex.ru>
2023-07-19 12:00:17 +0300

    создание таблиц в бд

--- data_base/sqlite_db.py
+++ data_base/sqlite_db.py
@@ -1,16 +1,25 @@
 import sqlite3 as sq
 
-
 base = sq.connect('bot.db')
 cur = base.cursor()
+
+
+def create_db():
+    start_db()
+    usage_record()
 
 
 def start_db():
     cur.execute("CREATE TABLE IF NOT EXISTS participant("
                 "id INTEGER PRIMARY KEY AUTOINCREMENT, "
                 "chat_id INTEGER, "
-                "user_name TEXT, "
+                "first_name TEXT, "
                 "user_id INTEGER, "
                 "num INTEGER)")
 
 
+def usage_record():
+    cur.execute("CREATE TABLE IF NOT EXISTS usage_record("
+                "id INTEGER PRIMARY KEY AUTOINCREMENT, "
+                "chat_id INTEGER, "
+                "usage_time TEXT)")
